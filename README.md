d0b3b3a4bb2f790e8240f8d4448dd496852fa200
Scelesto <Scelesto@users.noreply.github.com>
2015-04-29 15:26:12 -0400

    v1.1
    Whole score of new features and debugging.  See readme.

--- README.md
+++ README.md
@@ -1,13 +1,51 @@
-# ExtSubmit
-
+# ExtSubmit JavaScript Plugin
 ##About
 Upload files and submit forms without redirecting the page, even in browsers without fancy APIs!
 
-Works in all modern browsers (anything that supports iframe)
+Many advanced browsers can upload files or submit forms by using AJAX, but many users on old-fashioned browsers don't have the necessary API.
 
-Works in all cases where the form is part of the page's initial HTML.  If the form is loaded dynamically with AJAX, this function will lose a certain degree of browser compatibility.
+This is a very easy solution which works for far more users, which allows forms to be submitted on the page.
 
-## How to use:
+Works in all modern browsers (anything that supports iframe and javascript) and is only 1.5 KB.
+##Basic Use:
+**Submit a form without redirecting**
+
+	<form ... redirect="false">
+	...
+	</form>
+*Can be submitted in any way*
+****
+**Submit the form and then do something**
+
+	<form ... redirect="false">
+	...
+	</form>
+	<script>
+	something.onclick = function(){ //or any other trigger
+		form_element.submit(function(){
+			...
+		});
+	}
+	</script>
+
+or, without the redirect attribute:
+
+	<form>
+	...
+	</form>
+	<script>
+	something.onclick = function(){ //or any other trigger
+		_eS(form_element,function(){
+			...
+		})
+	}
+	</script>
+
+*Must be submitted in JavaScript*
+
+****
+
+## Calling Directly in JavaScript:
 
 **Submit a form in the background without redirecting**
 
@@ -15,6 +53,8 @@ Works in all cases where the form is part of the page's initial HTML.  If the fo
 	extSubmit("form_id");
 
 *for general use*
+
+****
 
 **Submit the form and then do something**
 
@@ -27,10 +67,36 @@ Works in all cases where the form is part of the page's initial HTML.  If the fo
 
 *for general use*
 
+****
+
 **Wait until the code is finished processing to continue**
 
 	extSubmit(something,true);
 
-*for systems that require a great deal of accuracy*
+*for developers*
+
+****
 
 It's that easy!
+## Tips:
+ExtSubmit only submits your form without redirecting.  It will be less obvious that you have messed up your code and the page does not redirect.
+
+Thus, if you have a problem with ExtSubmit, **please check your own code first.**  I can't guarantee that ExtSubmit will work unless you are following good development practices.
+****
+ExtSubmit works in all cases where the form is part of the page's initial HTML.  If the form is loaded dynamically with AJAX, ExtSubmit will partially stop working.
+
+If you call the function directly in JavaScript, it will still work, however it may lose a certain degree of browser compatibility.
+
+If this causes you trouble, load the ExtSubmit script **after the form has been loaded to the page.**
+****
+Some of you who are used to submitting by JavaScript may be used to doing this:
+
+	<form ... onsubmit="return false">
+	...
+	</form>
+This prevents the form from submitting.  If you do this with extSubmit, it will still prevent the form from submitting.  You can call other functions with onsubmit, for instance:
+
+	<form ... onsubmit="alert()">
+	...
+	</form>
+However, if you want your form to submit, **please do not make onsubmit return false.**
--- extSubmit.js
+++ extSubmit.js
@@ -1,62 +1,79 @@
-function extSubmit(value, wait) {
-    if (typeof(wait) == 'undefined') {
-        wait = false;
-    }
-    var func = function() {
-        var upl = function(object) {
-            if (object['form'].constructor == String) {
-                object['form'] = document.getElementById(object['form']);
+window._eS=function(form,done){
+	if(!window.FormData){
+		var iframe = document.createElement('iframe'),
+			loaded;
+		iframe.style.display = 'none';
+		iframe.src = window.location.href.replace(/[^#?]*\//,'');
+		document.body.appendChild(iframe);
+		loaded = document.getElementsByTagName('iframe')[0];
+		loaded.onload = function(){
+			var content = loaded.contentDocument,
+				allForms = content.getElementsByTagName('form'),
+				replacement,
+				i = 0;
+			for(;i<allForms.length;++i){
+				if(allForms[i].isEqualNode(form.cloneNode(true))){
+					replacement = allForms[i];
+					break;
+				}
+			}
+			form.parentNode.replaceChild(form.cloneNode(true),form);
+			replacement.parentNode.replaceChild(form,replacement);
+			content.getElementsByTagName('form')[i].submit();
+			loaded.onload = typeof(done)!='undefined'?done:function(){};
+		}
+	} else {
+		var ajax = new XMLHttpRequest();
+		ajax.open(form.method?form.method:'post', form.action?form.action:'', true);
+		if(typeof(done)!='undefined'){
+			ajax.onload = function(){
+				if(ajax.status==200){
+					done();
+				}
+			}
+		}
+		ajax.send(new FormData(form));
+		form.parentNode.replaceChild(form.cloneNode(form),form);
+	}
+}
+window.extSubmit=function(value,wait){
+    var func = function(){
+        (function(object){
+            if(object.form.constructor==String){
+                object.form = document.getElementById(object.form);
             }
-            if (!window.FormData) {
-                var iframe = document.createElement('iframe'),
-                    loaded;
-                iframe.style.display = 'none';
-                iframe.src = window.location.href.replace(/[^#?]*\//, '');
-                document.body.appendChild(iframe);
-                loaded = document.getElementsByTagName('iframe')[0];
-                loaded.onload = function() {
-                    var upload = object['form'],
-                        content = loaded.contentDocument,
-                        allForms = content.getElementsByTagName('form'),
-                        replacement,
-                        i = 0;
-                    for (; i < allForms.length; ++i) {
-                        if (allForms[i].isEqualNode(upload.cloneNode(true))) {
-                            replacement = allForms[i];
-                            break;
-                        }
-                    }
-                    upload.parentNode.replaceChild(upload.cloneNode(true), upload);
-                    replacement.parentNode.replaceChild(upload, replacement);
-                    content.getElementsByTagName('form')[i].submit();
-                    loaded.onload = object['done'] ? object['done'] : function() {};
-                }
-            } else {
-                var upload = object['form'],
-                    ajax = new XMLHttpRequest();
-                ajax.open(upload.method ? upload.method : 'post', upload.action ? upload.action : '', true);
-                if (object['done']) {
-                    ajax.onload = function() {
-                        if (ajax.status == 200) {
-                            object['done']();
-                        }
-                    }
-                }
-                ajax.send(new FormData(upload));
-                upload.parentNode.replaceChild(upload.cloneNode(true), upload);
-            }
-        };
-        if (value.constructor == Object) {
-            upl(value);
-        } else {
-            upl({
-                form: value
-            });
-        }
+            _eS(object.form,object.done);
+        })(value.constructor==Object?value:{
+			form:value
+		});
     };
-    if (wait) {
+    if(typeof(wait)!='undefined'&&wait==true){
         func();
     } else {
         setTimeout(func, 0);
     }
+}
+HTMLFormElement.prototype.oldSubmit=HTMLFormElement.prototype.submit;
+HTMLFormElement.prototype.submit=function(done){
+	if((this.attributes.redirect?this.attributes.redirect.value:'true')=='true'){
+		this.oldSubmit();
+	}else{
+		_eS(this,done);
+	}
+}
+document.body.onload=function(){
+	var forms=document.getElementsByTagName('form'),
+		i=0;
+	for(;i<forms.length;++i){
+		if((forms[i].attributes.redirect?forms[i].attributes.redirect.value:'true')!='true'){
+			forms[i].oldOnSubmit=forms[i].onsubmit?forms[i].onsubmit:function(){};
+			forms[i].onsubmit=function(event){
+				event.preventDefault();
+				if(this.oldOnSubmit()!==false){
+					this.submit()
+				}
+				return false;
+			}
+		}
+	}
 }
